69b4bd9c3e12d231be1803022d346556a3a0e3f1
sohan-99 <sohan75632@gmail.com>
2023-11-13 20:31:51 +0600

    Show Popular menu items in the home page1~

---
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuItem from "../../Share/MenuItem/MenuItem";
+
+
+const PopularMenu = () => {
+    const [menu, setmenu] = useState([])
+    useEffect(() => {
+        fetch('menu.json')
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                const popularitems = data.filter(item => item.category === 'popular');
+                setmenu(popularitems)
+            })
+    })
+    return (
+        <section className="mb-12">
+            <SectionTitle
+                heading={"From Our Menu"}
+                subheading={"Popular Items"}
+            >
+            </SectionTitle>
+
+            <div className="grid md:grid-cols-2 gap-10">
+                {
+
+                menu.map(item=> <MenuItem
+                 key={item._id}
+                 item ={item}
+                  ></MenuItem>)
+                }
+            </div>
+        </section>
+    );
+};
+
+export default PopularMenu;
