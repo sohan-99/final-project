cee6b02648a05df4f27ae6e93dc6c0677471e20b
sohan-99 <sohan75632@gmail.com>
2023-11-13 21:12:49 +0600

    parraParallax

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -4,9 +4,9 @@ import './featured.css'
 
 const Featured = () => {
     return (
-        <div className="featured-items text-white pt-8 my-20">
+        <div className="featured-items text-white bg-fixed pt-8 my-20">
             <SectionTitle subheading={'FEATURED ITEM'} heading={ '---Check it out---'}></SectionTitle>
-           <div className="md:flex justify-center items-center px-36 pb-20 pt-12">
+           <div className="md:flex justify-center items-center px-36 pb-20 pt-12 bg-slate-500 bg-opacity-40 space-x-3">
             <div>
 
             <img src={featuredimg} alt="" />
@@ -15,7 +15,7 @@ const Featured = () => {
                 <p className="md:ml-10">March 20, 2023</p>
                 <p className="uppercase">WHERE CAN I GET SOME?</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptate facere, deserunt dolores maiores quod nobis quas quasi.Eaque repellat recusandae add laudantium tempore consequatur consequuntur omnis ullam maxime tenetur.</p>
-                <button className="btn btn-active">Order Now</button>
+                <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
             </div>
            </div>
         </div>
