777cd16cb3baeb497d2a70703164c1f97fc860ef
sohan-99 <sohan75632@gmail.com>
2023-11-13 16:15:49 +0600

    project setup done

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Main from './Layout/Main';
+import Home from './pages/Home/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div className='max-w-screen-xl mx-auto'>
+
+     <RouterProvider router={router} />
+    </div>
+  </React.StrictMode>,
+)
