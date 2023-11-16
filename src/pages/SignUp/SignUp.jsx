af182bfb3fd8d8aa485644520dcb857a20db4d92
sohan-99 <sohan75632@gmail.com>
2023-11-17 00:40:19 +0600

    Explore React Hook Form Validation, Error Handling

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,8 +1,16 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
-
 const SignUp = () => {
+    const {register,  handleSubmit, watch, formState: { errors }} = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data)}
+        // console.log(watch("example")) // watch input value by passing the name of it
+
     return (
         <div className="my-6">
             <div className="mt-14 mb-9 flex flex-col items-center justify-center">
@@ -12,41 +20,38 @@ const SignUp = () => {
                     </h1>
                     <h4 className="text-center text-lg font-medium">Create a new account</h4>
                     <form
-                        //  onSubmit={handRegister} 
+                        onSubmit={handleSubmit(onSubmit)}
                         className="space-y-4">
                         <div>
                             <label className="block text-black lg:text-lg md:text-base font-medium">Name</label>
                             <input
                                 type="text"
+                                {...register("name")}
                                 name="name"
                                 placeholder="Name"
+                               
                                 className="w-full border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-400"
+                                // {errors.name && <span>This field is required</span>}
+
                                 required
                             />
                         </div>
-                        {/* <div>
-                            <label className="block text-black lg:text-lg md:text-base   font-medium">Photo URL</label>
-                            <input
-                                type="text"
-                                name="photo"
-                                placeholder="Photo URL"
-                                className="w-full border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-400"
-                            />
-                        </div> */}
                         <div>
                             <label className="block text-black lg:text-lg md:text-base font-medium">Email</label>
                             <input
                                 type="email"
+                                {...register("email")}
                                 name="email"
                                 placeholder="Email"
                                 className="w-full border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-400"
-                                required
+                                // required
                             />
                         </div>
                         <div>
                             <label className="block text-black lg:text-lg md:text-base  font-medium">Password</label>
                             <input
                                 type="password"
+                                {...register("password")}
                                 name="password"
                                 placeholder="Password"
                                 className="w-full border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-400"
