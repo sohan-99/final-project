30d687d794a0b067f42ff5e875b36feb7c257f77
sohan-99 <sohan75632@gmail.com>
2023-11-20 20:28:14 +0600

    Delete menu item with VerifyAdmin middleware

--- src/pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/pages/Dashboard/ManageItems/ManageItems.jsx
@@ -2,10 +2,44 @@ import { FaEdit, FaTrashAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import useMenu from "../../../hooks/useMenu";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 
 const ManageItems = () => {
     const [menu, , refetch] = useMenu();
+    const axiosSecure = useAxiosSecure();
+
+    const handleDeleteItem = (item) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                const res = await axiosSecure.delete(`/menu/${item._id}`);
+                console.log(res.data);
+                if (res.data.deletedCount > 0) {
+                    // refetch to update the ui
+                    refetch();
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: `${item.name} has been deleted`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+
+
+            }
+        });
+    }
+
     return (
         <div>
         <SectionTitle heading="Manage All Items" subHeading="Hurry up"></SectionTitle>
@@ -55,7 +89,7 @@ const ManageItems = () => {
                                 </td>
                                 <td>
                                     <button
-                                        // onClick={() => handleDeleteItem(item)}
+                                        onClick={() => handleDeleteItem(item)}
                                         className="btn btn-ghost btn-lg">
                                         <FaTrashAlt className="text-red-600"></FaTrashAlt>
                                     </button>
