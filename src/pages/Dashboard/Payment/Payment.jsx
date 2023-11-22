7ebc391d8d4234a9919ca234c288466aae0cb79a
sohan-99 <sohan75632@gmail.com>
2023-11-22 19:13:44 +0600

    Save payment info and delete cart items in the database

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -4,7 +4,9 @@ import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckoutForm";
 
 // TODO: add publishable key
-const stripePromise = loadStripe(`import.meta.env.VITE_Payment_Gateway_PK`);
+// const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
+const stripePromise = loadStripe('pk_test_51OFB4pGnPK8wwOFYTchIjW0jCh7PV2BPkC8neYvtl3p8QzMcCgkwjH3tmPpYM3pD991heVZ6siGjkdeaXVrxWztI00b1pnKJPj');
+console.log(stripePromise);
 const Payment = () => {
     return (
         <div>
